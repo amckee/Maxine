01df9e691fdc09d1ba8c0bdec8e97b245bd4edc8
Adam McKee <armware+gitstuff@gmail.com>
2018-05-31 20:57:59 -0500

    created sandbox to fix fan module

--- fan_sandbox.py
+++ fan_sandbox.py
@@ -8,10 +8,10 @@ fan = LED(4)
 fan.off()
 
 #temp at which we turn the fan on
-maxtemp = 70
+maxtemp = 61
 
 #temp at which we turn the fan off
-mintemp = 55
+mintemp = 60
 
 def getTemp():
     cmd = "vcgencmd measure_temp | grep -Eo '[0-9]{1,3}\.[0-9]'"
@@ -21,29 +21,28 @@ def getTemp():
 # for some reason, fan.on turns the fan off, and fan.off turns the fan on
 # to keep the code readable i've made these functions to handle this shit
 def fanOn():
-    if fan.is_active:
-        print("Turning fan on.")
-        fan.off()
-def fanOff():
     if not fan.is_active:
-        print("Turning fan off.")
+        print("Turning fan on.")
         fan.on()
+def fanOff():
+    if fan.is_active:
+        print("Turning fan off.")
+        fan.off()
 
 def controlFan(temp):
-    print("Tempurature: %s" % temp)
-    print("Fan Value: %s" % fan.value)
-    print("Is active: %s" % fan.is_active)
-    if temp >= maxtemp:
+    print("Temperature: %s - %s/%s" % (temp, mintemp, maxtemp))
+    print("fan.value: %s" % fan.value)
+    print("fan.is_active: %s" % fan.is_active)
+
+    if int(temp) >= int(maxtemp):
         fanOn()
     elif temp <= mintemp:
         fanOff()
 
 def main():
-    #print("main()")
     while True:
         controlFan(getTemp())
         time.sleep(1)
-
 
 # start of program.
 main()
